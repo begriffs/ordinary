56a149184a5394313d2cc10624fdcdff3921ad70
Joe Nelson and Ian Cordasco <dev+begriffs+sigmavirus24@bendyworks.com>
2013-06-14 15:32:35 -0500

    Addition up to omega and comparison

--- lib/ordinary.rb
+++ lib/ordinary.rb
@@ -1,47 +1,76 @@
 require "ordinary/version"
 
 module Ordinary
-  #class Infinity < Ord
-  #end
-
-  #def Infinity
-  #  Ord.new specialvalue
-  #end
-
   class Ord
     attr_reader :value
 
     def initialize(value)
-      @value = value
+      if value.is_a? Fixnum
+        @value = [{coef: value, exp: 0}]
+      elsif value == :omega
+        @value = [{coef: 1, exp: 1}]
+      elsif value.is_a? Array
+        @value = value
+      elsif value.is_a? Hash
+        @value = [value]
+      elsif value.is_a? Ord
+        @value = value.value
+      else
+        raise ArgumentError
+      end
+    end
+
+    def self.Omega
+      Ord.new :omega
     end
 
     def +(other)
-      if other.is_a? Ord
-        other = other.value
+      if other == Ord.Omega
+        return Ord.Omega
       end
-      Ord.new(@value + other)
+      other = Ord.new other
+      unmatched_theirs = other.value.clone
+      unmatched_ours = @value.clone
+      result = []
+      @value.each do |term|
+        matched = nil
+        unmatched_theirs.delete_if do |t2|
+          matched = t2 if term[:exp] == t2[:exp]
+        end
+        if matched
+          unmatched_ours.delete term
+          result.push({ coef: term[:coef] + matched[:coef], exp: term[:exp] })
+        end
+      end
+      Ord.new(result.concat(unmatched_ours.concat unmatched_theirs))
     end
 
-    def -(other)
-      if other.is_a? Ord
-        other = other.value
-      end
-      raise ArgumentError if other > @value
-      Ord.new(@value - other)
-    end
+    def <=>(other)
+      other = Ord.new other
 
-    def *(other)
-      if other.is_a? Ord
-        other = other.value
+      ours_sorted = @value.sort &term_compare(:desc)
+      theirs_sorted = other.value.sort &term_compare(:desc)
+      ours_sorted.each do |term|
+        return 1 if theirs_sorted.empty?
+        battle = term_compare.call term, theirs_sorted.first
+        return battle if battle != 0
+        theirs_sorted.pop
       end
-      Ord.new(@value * other)
+      return 0
     end
 
     def ==(other)
-      if other.respond_to? :value
-        other = other.value
+      (self <=> other) == 0
+    end
+
+    private
+
+    def term_compare direction=:asc
+      Proc.new do |t, u|
+        v = (2 * (t[:exp] <=> u[:exp])) + (t[:coef] <=> u[:coef])
+        v /= (v != 0 ? v.abs : 1)
+        (direction == :asc) ? v : -v
       end
-      other == @value
     end
   end
 
--- spec/ordinary_spec.rb
+++ spec/ordinary_spec.rb
@@ -3,17 +3,17 @@ include Ordinary
 
 describe Ordinary do
   describe Ord do
+    before :each do
+      @two = Ord.new 2
+      @four = Ord.new 4
+      @six = Ord.new 6
+    end
+
     it 'should provide an Ord class' do
       Ord.should be
     end
 
     context 'given a Fixnum' do
-      before :each do
-        @two = Ord.new 2
-        @four = Ord.new 4
-        @six = Ord.new 6
-      end
-
       it 'returns an Ord after addition' do
         expect(@two + 2).to be_instance_of(Ord)
       end
@@ -33,18 +33,27 @@ describe Ordinary do
       it 'compares against Fixnums on the right' do
         2.should == @two
       end
+    end
 
-      it 'handles multiplication' do
-        (@two * 2).should == @four
-        (@two * 3).should == @six
+    context 'with the first infinity' do
+      it 'addition dwarfs any finite number' do
+        (@two + Ord.Omega).should == Ord.Omega
       end
 
-      it 'refuses to subtract larger from smaller' do
-        lambda { @two - @six }.should raise_error(ArgumentError)
+      it 'handles right addition differently' do
+        (Ord.Omega + @two).should_not == Ord.Omega
       end
 
-      it 'handles subtraction' do
-        (@six - @two).should == @four
+      it 'compares finite numbers' do
+        (@two <=> @four).should == -1
+        (@four <=> @two).should == 1
+        (@four <=> @four).should == 0
+      end
+
+      it 'compares omega against finite numbers' do
+        (@two <=> Ord.Omega).should == -1
+        (Ord.Omega <=> @two).should == 1
+        (Ord.Omega <=> Ord.Omega) == 0
       end
     end
   end
